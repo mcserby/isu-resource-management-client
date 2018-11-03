7088ce84b32e47fd3eb9063f3c6f638c396f4a87
acentea <andrei_centea@yahoo.com>
2018-11-03 17:00:38 +0200

    Just a git test

--- test/unit/specs/Main.spec.js
+++ test/unit/specs/Main.spec.js
@@ -6,6 +6,6 @@ describe('Main.vue', () => {
     const Constructor = Vue.extend(Main)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+      .to.equal('Welcome to Your Vue.js Isu App')
   })
 })
