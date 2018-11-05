7468d0f0dfa2b951c2964c81be7a5ec0c1e7f523
mserban <mihai-ioan.serban@frequentis.com>
2018-11-05 12:35:53 +0200

    main page styling

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,16 @@
 <template>
-  <div id = "content">
-    <router-link to="/principal">Go to Principal</router-link>
-    <router-link to="/services">Go to Services</router-link>
-    <router-link to="/uat">Go to UAT</router-link>
+  <div class="main-wrapper">
+<!--    <div id="headline">
+      ISU - Managementul resurselor
+    </div>-->
+    <div class = "menu-wrapper">
+      <div id="headline">
+        ISU - Managementul resurselor
+      </div>
+      <router-link class="btn menu-link" role="button" to="/principal">Modulul Principal</router-link>
+      <router-link class="btn menu-link" role="button" to="/services">Modulul Servicii</router-link>
+      <router-link class="btn menu-link" role="button" to="/uat">Modulul UAT</router-link>
+    </div>
   </div>
 </template>
 
@@ -11,3 +19,4 @@ export default {
   name: 'Main',
 }
 </script>
+<style src="./main.css"></style>
