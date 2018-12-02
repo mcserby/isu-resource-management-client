386ee543aad775ac7c04f64c3f1ad44f180c55d3
AndreeaCh <andreeachiscari@gmail.com>
2018-12-02 11:46:44 +0200

    wip.

---
+++ src/components/principal/unit/resource/StatusSelectionMenu.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="menu" v-show="show" v-on-clickaway="close" @click="close">
+  <menu class="menu-options">
+    <menuitem class="menu-option menu-option-disponibil" onclick="setStatusToDisponibil()">Disponibil</menuitem>
+    <menuitem class="menu-option menu-option-misiune" onclick="setStatusToMisiune()">Misiune</menuitem>
+    <menuitem class="menu-option menu-option-indisponibil" onclick="setStatusToIndisponibil()">Indisponibil</menuitem>
+  </menu>
+</div>
+</template>
+<script>
+export default {
+  name: 'StatusSelectionMenu',
+  props: [show],
+  methods: {
+    setStatusToDisponibil: function() {
+
+    },
+    setStatusToMisiune: function() {
+
+    },
+    setStatusToIndisponibil: function() {
+
+    },
+    close: function() {
+      this.show = false;
+    }
+  }
+}
+</script>
+<style src="./statusSelectionMenu.css"></style>
