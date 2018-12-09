2e88a36cbd67f60e998b3d2445582b4734c8a19f
Adrian Galos <adrian.galos@gmail.com>
2018-12-09 22:31:45 +0200

    sortResources: Sort resources based on status and type

--- src/store/modules/principal.js
+++ src/store/modules/principal.js
@@ -1,15 +1,30 @@
 import A from '../../constants/actions'
 import M from '../../constants/mutations'
 import Vue from 'vue'
-import Tab from '../../contracts/tab';
-import ResourceType from '../../constants/resourceType';
-import Resource from '../../contracts/resource';
+import Tab from '../../contracts/tab'
+import ResourceType from '../../constants/resourceType'
+import Resource from '../../contracts/resource'
+
+function sortResource (r1, r2) {
+  {
+    let res = state.statuses[r2.status.status] - state.statuses[r1.status.status]
+    if (res === 0) {
+      if (r1.vehicleType > r2.vehicleType) {
+        return 1
+      } else if (r2.vehicleType === r1.vehicleType) {
+        return 0
+      } else return -1
+    } else {
+      return res
+    }
+  }
+}
 
 const tabs = [
   new Tab('Tehnică de primă intervenție', ResourceType.FIRST_INTERVENTION),
   new Tab('Alte tipuri de tehnică', ResourceType.OTHER),
   new Tab('Echipamente', ResourceType.EQUIPMENT)
-];
+]
 
 const state = {
   units: [],
@@ -19,7 +34,8 @@ const state = {
   activeResource: null,
   resourceDialogIsOpen: false,
   confirmationDialogIsOpen: false,
-  resourceViewDialogIsOpen: false
+  resourceViewDialogIsOpen: false,
+  statuses: {'IN_MISSION': 2, 'AVAILABLE': 1, 'UNAVAILABLE': 0}
 }
 
 const actions = {
@@ -66,77 +82,80 @@ const actions = {
 
 const mutations = {
   [M.INIT_UNITS] (state, units) {
-    state.units.splice(0, state.units.length);
-    state.units = units;
+    state.units.splice(0, state.units.length)
+    state.units = units
+    units.forEach(u =>
+      u.resources.sort((r1, r2) => sortResource(r1, r2)))
   },
   [M.UNIT_UPDATED] (state, unit) {
-    let updatedUnit = state.units.find(u =>  u.name === unit.name);
+    let updatedUnit = state.units.find(u => u.name === unit.name)
     if (updatedUnit) {
-      Vue.set(updatedUnit, 'resources', unit.resources);
-      Vue.set(updatedUnit, 'lastUpdate', unit.lastUpdate);
+      unit.resources.sort((r1, r2) => sortResource(r1, r2))
+      Vue.set(updatedUnit, 'resources', unit.resources)
+      Vue.set(updatedUnit, 'lastUpdate', unit.lastUpdate)
     }
   },
   [M.CLEAR_UNIT_RESOURCES] (state, unitName) {
-    let unit = state.units.find(u =>  u.name === unitName)
+    let unit = state.units.find(u => u.name === unitName)
     if (unit) {
-      let currentResourceType =  state.activeTab.resourceType;
-      let updatedUnitResources = unit.resources.filter(r => r.type != currentResourceType);
+      let currentResourceType = state.activeTab.resourceType
+      let updatedUnitResources = unit.resources.filter(r => r.type != currentResourceType)
       Vue.set(unit, 'resources', updatedUnitResources)
     }
   },
   [M.OPEN_CONFIRMATION_DIALOG] (state, unitName) {
-    let unit = state.units.find(u => u.name === unitName);
-    if(unit){
-      state.activeUnit = unit;
-      state.confirmationDialogIsOpen = true;
+    let unit = state.units.find(u => u.name === unitName)
+    if (unit) {
+      state.activeUnit = unit
+      state.confirmationDialogIsOpen = true
     }
   },
   [M.CLOSE_CONFIRMATION_DIALOG] (state) {
-    state.confirmationDialogIsOpen = false;
+    state.confirmationDialogIsOpen = false
   },
- [M.OPEN_VIEW_RESOURCE_DIALOG] (state, resource) {
+  [M.OPEN_VIEW_RESOURCE_DIALOG] (state, resource) {
     for (let unit of state.units) {
       let res = unit.resources.find(r => r === resource)
       if (res) {
-        state.activeUnit = unit;
-        state.activeResource = resource;
-        state.resourceViewDialogIsOpen = true;
+        state.activeUnit = unit
+        state.activeResource = resource
+        state.resourceViewDialogIsOpen = true
       }
     }
-},
-  [M.CLOSE_VIEW_RESOURCE_DIALOG] (state) {
-    state.resourceViewDialogIsOpen = false;
   },
-[M.CLOSE_CONFIRMATION_DIALOG] (state) {
-  state.confirmationDialogIsOpen = false;
-},
+  [M.CLOSE_VIEW_RESOURCE_DIALOG] (state) {
+    state.resourceViewDialogIsOpen = false
+  },
+  [M.CLOSE_CONFIRMATION_DIALOG] (state) {
+    state.confirmationDialogIsOpen = false
+  },
   [M.OPEN_ADD_RESOURCE_DIALOG] (state, unitName) {
-    let unit = state.units.find(u => u.name === unitName);
-    if(unit){
-      state.activeUnit = unit;
-      state.resourceDialogIsOpen = true;
+    let unit = state.units.find(u => u.name === unitName)
+    if (unit) {
+      state.activeUnit = unit
+      state.resourceDialogIsOpen = true
     }
   },
   [M.CLOSE_ADD_RESOURCE_DIALOG] (state) {
-    state.resourceDialogIsOpen = false;
+    state.resourceDialogIsOpen = false
   },
   [M.ADD_RESOURCE] (state, resource) {
-    state.activeUnit.resources.push(resource);
+    state.activeUnit.resources.push(resource)
   },
   [M.LOCK_UNIT] (state, unitName) {
-    let unit = state.units.find(u => u.name === unitName);
+    let unit = state.units.find(u => u.name === unitName)
     if (unit) {
-      Vue.set(unit, 'isLocked', true);
+      Vue.set(unit, 'isLocked', true)
     }
   },
   [M.UNLOCK_UNIT] (state, unitName) {
-    let unit = state.units.find(u => u.name === unitName);
+    let unit = state.units.find(u => u.name === unitName)
     if (unit) {
-      Vue.set(unit, 'isLocked', false);
+      Vue.set(unit, 'isLocked', false)
     }
   },
   [M.CHANGE_ACTIVE_TAB] (state, tab) {
-    state.activeTab = tab;
+    state.activeTab = tab
   },
 }
 
