8fb915b684faa6e5a0fb8d62e2a9e5e6b3fbd7ef
AndreeaCh <andreeachiscari@gmail.com>
2018-12-12 00:01:05 +0200

    Show updated crew list when in mission.

--- src/components/principal/unit/form/ResourceDialog.vue
+++ src/components/principal/unit/form/ResourceDialog.vue
@@ -10,34 +10,65 @@
           <div class="modal-body">
             <div class="form-group">
               <label class="form-label" for="name">Detașament</label>
-              <input type="text" v-model="unit.name" class="form-control" id="unit" readonly="true" placeholder="Detașament">
+              <input
+                type="text"
+                v-model="unit.name"
+                class="form-control"
+                id="unit"
+                readonly="true"
+                placeholder="Detașament"
+              >
             </div>
             <div class="form-group">
               <label class="form-label" for="name">Tip autospecială</label>
-              <input type="text" v-model="resource.vehicleType" class="form-control" id="name" readonly="true" placeholder="Tip autospecială">
+              <input
+                type="text"
+                v-model="resource.vehicleType"
+                class="form-control"
+                id="name"
+                readonly="true"
+                placeholder="Tip autospecială"
+              >
             </div>
             <div class="form-group">
               <label class="form-label" for="plateNumber">Număr de înmatriculare</label>
-              <input type="text" minlength="5" class="form-control" id="plateNumber" aria-describedby="plateNumberHelp" placeholder="Număr de înmatriculare" v-model="resource.plateNumber" readonly="true">
+              <input
+                type="text"
+                minlength="5"
+                class="form-control"
+                id="plateNumber"
+                aria-describedby="plateNumberHelp"
+                placeholder="Număr de înmatriculare"
+                v-model="resource.plateNumber"
+                readonly="true"
+              >
             </div>
             <div class="form-group">
               <label class="form-label" for="identificationNumber">Număr de identificare</label>
-              <input minlength="2" type="text" class="form-control"  id="identificationNumber" placeholder="Număr de identificare" v-model="resource.identificationNumber" readonly="true">
+              <input
+                minlength="2"
+                type="text"
+                class="form-control"
+                id="identificationNumber"
+                placeholder="Număr de identificare"
+                v-model="resource.identificationNumber"
+                readonly="true"
+              >
             </div>
             <div class="form-group">
               <label class="form-label" for="crew">Echipaj</label>
-              <textarea minlength="5" class="form-control" id="crew" rows="3" readonly="true" v-model="this.crewMembers"></textarea>
+              <textarea
+                minlength="5"
+                class="form-control"
+                id="crew"
+                rows="3"
+                readonly="true"
+                v-model="this.crewMembers"
+              ></textarea>
             </div>
-
           </div>
           <div class="modal-footer">
-            <button
-              type="button"
-              class="btn custom-button"
-              @click="cancel"
-            >
-              Închide
-            </button>
+            <button type="button" class="btn custom-button" @click="cancel">Închide</button>
           </div>
         </div>
       </div>
@@ -46,25 +77,29 @@
 </template>
 
 <script>
-
-import A from '../../../../constants/actions'
-import Resource from '../resource/Resource.vue'
+import A from "../../../../constants/actions";
+import Resource from "../resource/Resource.vue";
+import ResourceStatus from "../../../../constants/resourceStatus.js";
 
 export default {
-  name: 'ResourceDialog',
-  props: ['resource', 'unit'],
+  name: "ResourceDialog",
+  props: ["resource", "unit"],
   data: () => {
     return {
-      crewMembers: ''
-    }
+      crewMembers: ""
+    };
   },
   methods: {
-    cancel: function () {
+    cancel: function() {
       this.$store.dispatch(A.CLOSE_VIEW_RESOURCE_DIALOG);
     },
-    parseCrewMembers: function () {
-      this.crewMembers = this.resource.crew.join('\n')
-      this.crewMembers = this.resource.captain + '\n' + this.crewMembers
+    parseCrewMembers: function() {
+      if (this.resource.status.status === ResourceStatus.IN_MISSION) {
+        this.crewMembers = this.resource.status.crew.join("\n");
+      } else {
+        this.crewMembers = this.resource.crew.join("\n");
+        this.crewMembers = this.resource.captain + "\n" + this.crewMembers;
+      }
     }
   },
 
@@ -72,10 +107,10 @@ export default {
     Resource
   },
 
-  created () {
-    this.parseCrewMembers()
+  created() {
+    this.parseCrewMembers();
   }
-}
+};
 </script>
 
 <style src="./addResourceForm.css"></style>
