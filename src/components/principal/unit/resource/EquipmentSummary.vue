39ceac58f23cf3fb48fbc91e870a40a24c2c78f0
Adrian Galos <adrian.galos@gmail.com>
2018-12-15 18:36:59 +0200

    Added style for equipment

--- src/components/principal/unit/resource/EquipmentSummary.vue
+++ src/components/principal/unit/resource/EquipmentSummary.vue
@@ -9,12 +9,13 @@
       <div class="equipment-usable-summary">
         <div class="equipment-element-container">{{equipment.usable}}</div>
       </div>
-      <div class="equipment-unusable-summary">
-        <div class="equipment-element-container">{{equipment.unusable}}</div>
-      </div>
       <div class="equipment-reserves-summary">
         <div class="equipment-element-container">{{equipment.reserves}}</div>
       </div>
+      <div class="equipment-unusable-summary">
+        <div class="equipment-element-container">{{equipment.unusable}}</div>
+      </div>
+
   </div>
 </template>
 
