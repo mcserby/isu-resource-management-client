59a79e18db0410f3641cff7b65465865e55325c7
mserban <mihai-ioan.serban@frequentis.com>
2019-01-21 23:02:33 +0200

    fixed resource editing bug, added resource id if not set by backend

--- src/contracts/resource.js
+++ src/contracts/resource.js
@@ -1,8 +1,8 @@
 // Resource contract
 
 class Resource {
-  constructor(vehicleType, plateNumber, identificationNumber, captain, crew, status, type) {
-    this.id = createUUID();
+  constructor(id, vehicleType, plateNumber, identificationNumber, captain, crew, status, type) {
+    this.id = id;
     this.vehicleType = vehicleType;
     this.plateNumber = plateNumber;
     this.identificationNumber = identificationNumber;
@@ -11,16 +11,6 @@ class Resource {
     this.status = status;
     this.type = type;
   }
-}
-
-function createUUID(){
-  var dt = new Date().getTime();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = (dt + Math.random()*16)%16 | 0;
-    dt = Math.floor(dt/16);
-    return (c==='x' ? r :(r&0x3|0x8)).toString(16);
-  });
-  return uuid;
 }
 
 export default Resource;
