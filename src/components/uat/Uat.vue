0574d1b5cbb67ef7ca1a5b07695593736b9e0d61
Serban Mihai <mcserby@gmail.com>
2019-09-21 17:55:22 +0300

    UAT module with open layers map

--- src/components/uat/Uat.vue
+++ src/components/uat/Uat.vue
@@ -1,9 +1,47 @@
 <template>
-    <h1>UAT</h1>
+<div style="background-color: red">
+  <h1>UAT</h1>
+  <div id="map"></div>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'UAT',
+import Map from 'ol/Map';
+import View from 'ol/View.js';
+import GeoJSON from 'ol/format/GeoJSON.js';
+import Circle from 'ol/geom/Circle.js';
+import {
+  Tile as TileLayer,
+  Vector as VectorLayer
+} from 'ol/layer.js';
+import {
+  OSM
+} from 'ol/source.js';
+
+
+export default {
+  name: 'UAT',
+
+  computed: {
+
+  },
+  methods: {
+
+  },
+  mounted: function() {
+    var map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      target: 'map',
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+    console.log("UAT module mounted");
   }
+}
 </script>
