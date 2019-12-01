4be80df881c67868a3474254521228473e2f4484
Marius Barta <marius.barta@gmail.com>
2019-12-01 22:26:30 +0200

    Including keycloak realm imports into the build process. Increased version.

--- static/env.js
+++ static/env.js
@@ -2,6 +2,6 @@
   window.isuEnv = {
     serverUrl: 'http://localhost:8345/',
     authenticationRequired: true,
-    authServiceUrl: '10.17.3.50:30002'
+    authServiceUrl: 'localhost:8180'
   };
 }(this));
